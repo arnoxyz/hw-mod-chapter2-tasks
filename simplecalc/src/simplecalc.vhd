fb1360ddb05eb8aa0c87a26b1a3922e531ccdb68
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2024-11-25 17:05:06 -0500

    init simplecalc

---
+++ simplecalc/src/simplecalc.vhd
@@ -0,0 +1,61 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity simplecalc is
+	generic (
+		DATA_WIDTH : natural := 8
+	);
+	port(
+		clk    : in std_ulogic;
+		res_n  : in std_ulogic;
+
+		operand_data_in : in std_ulogic_vector(DATA_WIDTH-1 downto 0);
+		store_operand1  : in std_ulogic;
+		store_operand2  : in std_ulogic;
+
+		sub : in std_ulogic;
+
+		operand1 : out std_ulogic_vector(DATA_WIDTH-1 downto 0);
+		operand2 : out std_ulogic_vector(DATA_WIDTH-1 downto 0);
+		result   : out std_ulogic_vector(DATA_WIDTH-1 downto 0)
+	);
+end entity;
+
+architecture arch of simplecalc is
+	signal local_op1 : std_ulogic_vector(DATA_WIDTH-1 downto 0);
+	signal local_op2 : std_ulogic_vector(DATA_WIDTH-1 downto 0);
+begin
+
+	--clk process with async low-active reset
+	sync : process(clk, res_n) 
+	begin 
+		if res_n='0' then
+			--reset all registers
+			local_op1 <= (others=>'0');			
+			local_op2 <= (others=>'0');			
+		else 
+			if rising_edge(clk) then 
+				--read new data logic
+				if store_operand1='1' then 
+					local_op1 <= operand_data_in;
+				end if;
+
+				if store_operand2='1' then 
+					local_op2 <= operand_data_in;
+				end if;
+
+				--calc and output logic
+				operand1 <= local_op1;
+				operand2 <= local_op2;
+
+				if sub='1' then 
+					result <= std_logic_vector(unsigned(local_op1) - unsigned(local_op2));
+				else 
+					result <= std_logic_vector(unsigned(local_op1) + unsigned(local_op2));
+				end if;
+			end if;
+		end if;
+	end process;
+end architecture;
---
+++ simplecalc/tb/simplecalc_tb.vhd
@@ -0,0 +1,96 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+entity simplecalc_tb is
+end entity;
+
+architecture bench of simplecalc_tb is
+	constant DATA_WIDTH : natural := 8;	
+
+	--clock stuff
+	constant clk_period : time := 2 ns; --50 ns =same as the fpga board clk
+	signal clk : std_ulogic := '0';
+	signal clk_stop : std_ulogic := '0';
+
+	--in signals
+	signal res_n  			: std_ulogic;
+	signal operand_data_in : std_ulogic_vector(DATA_WIDTH-1 downto 0);
+	signal store_operand1  : std_ulogic;
+	signal store_operand2  : std_ulogic;
+	signal sub 			: std_ulogic;
+
+	--out signals
+	signal operand1 : std_ulogic_vector(DATA_WIDTH-1 downto 0);
+	signal operand2 : std_ulogic_vector(DATA_WIDTH-1 downto 0);
+	signal result   : std_ulogic_vector(DATA_WIDTH-1 downto 0);
+
+	--component
+	component simplecalc is
+		generic (
+			DATA_WIDTH : natural := 8
+		);
+		port(
+			clk    : in std_ulogic;
+			res_n  : in std_ulogic;
+
+			operand_data_in : in std_ulogic_vector(DATA_WIDTH-1 downto 0);
+			store_operand1  : in std_ulogic;
+			store_operand2  : in std_ulogic;
+
+			sub : in std_ulogic;
+
+			operand1 : out std_ulogic_vector(DATA_WIDTH-1 downto 0);
+			operand2 : out std_ulogic_vector(DATA_WIDTH-1 downto 0);
+			result   : out std_ulogic_vector(DATA_WIDTH-1 downto 0)
+		);
+	end component;
+
+begin
+
+	-- Instantiate the unit under test
+	UUT : simplecalc
+	generic map( DATA_WIDTH => DATA_WIDTH)
+	port map(
+			clk    => clk,
+			res_n  => res_n,
+			operand_data_in => operand_data_in,
+			store_operand1 => store_operand1,
+			store_operand2 => store_operand2,
+			sub => sub,
+			operand1 => operand1,
+			operand2 => operand2,
+			result   => result
+	);
+
+	-- Stimulus process
+	stimulus: process
+	begin
+		report "simulation start";
+
+		-- Apply test stimuli
+		res_n <= '0';
+		wait for 50 ns;
+		clk_stop <= '1';
+
+		wait for 10 ns;
+		assert 1 = 0 report "Test x failed" severity error;
+
+		report "simulation end";
+		-- End simulation
+		wait;
+	end process;
+
+	-- Gen Clk
+	gen_clk : process
+	begin 
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+		if clk_stop='1' then 
+			wait;
+		end if;
+	end process;
+end architecture;
