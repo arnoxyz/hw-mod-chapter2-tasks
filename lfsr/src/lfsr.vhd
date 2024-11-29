370f7a2d37a27ca7f5f34ef0ee06f51403fd6105
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2024-11-29 07:47:24 -0500

    lfsr implement with same behavior as in demo2 lfsr, fixed values

--- lfsr/src/lfsr.vhd
+++ lfsr/src/lfsr.vhd
@@ -17,8 +17,64 @@ end entity;
 
 architecture arch of lfsr is
 	-- Add signals as required
+	signal x : std_ulogic_vector(LFSR_WIDTH-1 downto 0);
+	signal y : std_ulogic_vector(LFSR_WIDTH-1 downto 0);
 begin
+	--4 bit linear feedback register
+    process(clk, res_n) is 
+    begin 
+        if res_n = '0' then 
+			--async reset all ff to 0
+			x <= (others=>'0');
+        elsif rising_edge(clk) then 
 
-	-- Implement LFSR
+			-- special case for x(0) 
+				-- generate from polynomial
+				--loop through polynomial and xor if entry is '1' and do nothing if entry is '0'
+				--Poly wants a cracker = 0011
+				--x(0) <= x(3) xor x(2);
+				-- for i in POLYNOMIAL'range loop
+				-- 	report to_string(i);
+				-- 	-- if i = '1' then 
+				-- 	-- 	y(i) <= x(i);
+				-- 	-- end if;
+				-- end loop;
 
+				-- for i in POLYNOMIAL loop
+				-- 	if i = '1' then 
+				-- 		x(0) <= y(i) xor y(i-1);
+				-- 	end if;
+				-- end loop;
+
+				x(0) <= x(2) xor x(3);
+				--Polynomial "0001"
+				--x(0) <= x(3)
+			
+			-- other cases for x(1) until x(LFSR_WIDTH-1)
+				-- generate from LFSR_WIDTH, 
+				--	starting with i=1 until LFSR_WIDTH-1
+				-- 	x(i) <= x(i-1)
+				-- for i in 1 to LFSR_WIDTH-1 loop
+				-- 	report to_string(i);
+				-- 	x(i) <= x(i-1);
+				-- 	-- x(1) <= x(0);
+				-- 	-- x(2) <= x(1);
+				-- 	-- x(3) <= x(2);
+				-- end loop;
+				x(0) <= x(1);
+				x(1) <= x(0);
+				x(2) <= x(1);
+				x(3) <= x(2);
+
+			--seed stuff
+				--output seed
+				if load_seed_n = '1' then 
+					prdata <= x(LFSR_WIDTH-1);
+				end if;
+				-- apply seed 
+				if load_seed_n = '0' then 
+					x <= seed;
+				end if;
+        end if;
+    end process;
 end architecture;
--- lfsr/tb/lfsr_tb.vhd
+++ lfsr/tb/lfsr_tb.vhd
@@ -7,26 +7,50 @@ end entity;
 
 architecture tb of lfsr_tb is
 	signal clk, res_n : std_ulogic := '0';
+	signal stop_clk : std_logic := '0';
+	constant clk_period : time := 20 ns; --20ns = 50 MHz
 
+	--Meaning of Max_poly?
 	constant MAX_POLY_8  : std_ulogic_vector(7 downto 0)  := "10111000";
 	constant POLY_8      : std_ulogic_vector(7 downto 0)  := "10100100";
 	constant MAX_POLY_16 : std_ulogic_vector(15 downto 0) := "1101000000001000";
 	constant POLY_16     : std_ulogic_vector(15 downto 0) := "1101001100001000";
 
 	-- Change as required
-	constant POLYNOMIAL : std_ulogic_vector := MAX_POLY_16;
+	constant POLYNOMIAL : std_ulogic_vector := "0011"; --MAX_POLY_16;
 	constant LFSR_WIDTH : integer := POLYNOMIAL'LENGTH;
 
-	signal load_seed_n, en : std_ulogic;
-	signal seed, seq     : std_ulogic_vector(LFSR_WIDTH-1 downto 0) := (others => '0');
+	signal load_seed_n : std_ulogic;
+	signal seed: std_ulogic_vector(LFSR_WIDTH-1 downto 0) := (others => '0');
+	--en? signals
+	--signal en : std_ulogic;
+	--seq? signals
+	--signal seq     : std_ulogic_vector(LFSR_WIDTH-1 downto 0) := (others => '0');
+
 	signal prdata : std_ulogic;
 begin
 
 	stimulus : process is
 	begin
-
 		-- Reset your module and apply stimuli
+		res_n <= '0';
+		wait for 5*clk_period;
+		seed <= "0001"; 
+		load_seed_n <= '1';
 
+
+		res_n <= '1';
+		wait until rising_edge(clk);
+		
+		--apply seed
+		load_seed_n <= '0';
+		wait for clk_period;
+		wait for clk_period;
+		load_seed_n <= '1';
+		wait until rising_edge(clk);
+
+		wait for 15*clk_period;
+		stop_clk <= '1';
 		wait;
 	end process;
 
@@ -46,6 +70,12 @@ begin
 	clk_gen : process is
 	begin
 		-- generate a 50MHz clock signal
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+		if stop_clk = '1' then 
+			wait;
+		end if;
 	end process;
-
 end architecture;
